f52e5d5c04854f9377ba452f58730100d5f72cd2
Cole Brokamp <cole.brokamp@gmail.com>
2016-05-02 16:47:41 -0400

    add script to auto push pubs to CV and website

---
+++ push_pubs.sh
@@ -0,0 +1,21 @@
+# add updated pubs files and commit
+git add .
+echo 'Enter the commit message:'
+read commitMessage
+git commit -m "$commitMessage"
+git push
+read
+
+# pubs.html is symmlinked to website directory
+# change to this directory; change html and push new site
+cd ~/Documents/cole-brokamp.github.io
+cp index.html index.html.backup
+
+cat index_pre.html > index.html
+cat /Users/cole/Documents/CV\ and\ Cover\ Letters\ and\ Applications/CV/pubs.html >> index.html
+cat index_post.html >> index.html
+
+git add
+git commit -m "$commitMessage"
+git push
+read
