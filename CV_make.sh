1f08101a686b2e4d3bfcdf382aa76810751d24c7
Cole Brokamp <cole.brokamp@gmail.com>
2016-12-20 13:38:18 -0500

    add yaml to make script

--- CV_make.sh
+++ CV_make.sh
@@ -1,11 +1,13 @@
-# extract pubs from yaml file into html and tex files
+# extract info from yaml file into md and tex files
 R CMD BATCH --vanilla parse_pubs.R
+R CMD BATCH --vanilla parse_talks.R
+R CMD BATCH --vanilla parse_software.R
 
 # then latex the CV into a pdf
 "/usr/texbin/texfot" "/usr/texbin/pdflatex" cole-brokamp-cv.tex
 
 # clean up
-rm parse_pubs.Rout
+rm *.Rout
 # rm cole-brokamp-cv.{log,synctex.gz}
 
 # open cv
