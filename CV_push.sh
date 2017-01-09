4ca4cb83f135238ff06e26d8737a92e74b998fe7
Cole Brokamp <cole.brokamp@gmail.com>
2017-01-09 16:42:13 -0500

    typo in push script

--- CV_push.sh
+++ CV_push.sh
@@ -5,8 +5,7 @@ git commit -m "$commitMessage"
 git push
 
 # in case pubs, talks, or software are changed rebuild website and push to website S3 bucket
-R -e \"rmarkdown::render_site(input='/Users/cole/Documents/CV_website_more/colebrokamp.com',
-encoding = 'UTF-8')\"
+R -e "rmarkdown::render_site(input='/Users/cole/Documents/CV_website_more/colebrokamp.com',encoding = 'UTF-8')"
 
 
 aws s3 sync /Users/cole/Documents/CV_website_more/colebrokamp.com/_site/ s3://colebrokamp.com
