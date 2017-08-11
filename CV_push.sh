f7edfb9e647a63e2d24deef3dfcf8279e6903219
Cole Brokamp <cole.brokamp@gmail.com>
2017-08-11 13:59:30 -0400

    update push script to GH, not AWS

--- CV_push.sh
+++ CV_push.sh
@@ -1,11 +1,22 @@
+# add and commit all for CV repo
 git add .
 echo 'Enter the commit message:'
 read commitMessage
 git commit -m "$commitMessage"
 git push
 
-# in case pubs, talks, or software are changed rebuild website and push to website S3 bucket
-R -e "rmarkdown::render_site(input='/Users/cole/Documents/CV_website_more/colebrokamp.com',encoding = 'UTF-8')"
+# copy cv to website folder
+cp cole-brokamp-cv.pdf /Users/cole/Documents/CV_website_more/colebrokamp.com/cv.pdf
 
 
-aws s3 sync /Users/cole/Documents/CV_website_more/colebrokamp.com/_site/ s3://colebrokamp.com
+# move to website folder; build and push to website repo
+cd /Users/cole/Documents/CV_website_more/colebrokamp.com
+
+R -e "rmarkdown::render_site(encoding = 'UTF-8')"
+
+git add .
+git commit -m "$commitMessage"
+git push
+
+# open site to double check the updates
+open http://colebrokamp.com
