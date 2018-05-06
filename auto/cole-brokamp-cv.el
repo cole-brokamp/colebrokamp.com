435891954733e8f2334f5d5e850eb1c18dc01f1b
Cole Brokamp <cole.brokamp@gmail.com>
2018-05-05 20:51:18 -0400

    healhODODjth & place reviewj

---
+++ auto/cole-brokamp-cv.el
@@ -0,0 +1,28 @@
+(TeX-add-style-hook
+ "cole-brokamp-cv"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("res" "margin" "line")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("xcolor" "dvipsnames") ("hyperref" "unicode=true")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "pubs"
+    "talks"
+    "software"
+    "support"
+    "res"
+    "res10"
+    "xcolor"
+    "hyperref")
+   (LaTeX-add-environments
+    "list"))
+ :latex)
+
