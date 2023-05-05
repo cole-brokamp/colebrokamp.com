ed5e1d715850154b7e95174d3804147bc0c4d911
Cole Brokamp <cole.brokamp@gmail.com>
2023-05-05 14:15:40 -0400

    ignore files in site, progress on nih biosketch

---
+++ make.R
@@ -0,0 +1,13 @@
+callr::r(\(.) rmarkdown::render_site("encoding" = "UTF-8"))
+
+callr::r(\(.) rmarkdown::render("_cole-brokamp-cv.Rmd",
+                                output_format = "pdf_document",
+                                output_file = "cole-brokamp-cv.pdf"))
+
+callr::r(\(.) rmarkdown::render('_cole-brokamp-cv-peds-format.Rmd',
+                                output_format = 'word_document',
+                                output_file = "cole-brokamp-cv-peds-format.docx"))
+
+callr::r(\(.) rmarkdown::render("_cole-brokamp-nihbiosketch.Rmd",
+                                output_format = "pdf_document",
+                                output_file = "cole-brokamp-nih-biosketch.pdf"))
