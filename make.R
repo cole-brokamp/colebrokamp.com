1d08b1bf4ac147b46f37ed9a75e3af33c94dcb53
Cole Brokamp <cole.brokamp@gmail.com>
2023-05-08 23:51:52 -0400

    rename

--- make.R
+++ make.R
@@ -12,6 +12,6 @@ callr::r(\(.) rmarkdown::render(
   output_file = "cole-brokamp-cv-peds-format.docx"))
 
 callr::r(\(.) rmarkdown::render(
-  "_cole-brokamp-nihbiosketch.Rmd",
+  "_cole-brokamp-nih-biosketch.Rmd",
   output_format = "pdf_document",
   output_file = "cole-brokamp-nih-biosketch.pdf"))
