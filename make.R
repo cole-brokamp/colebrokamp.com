7716196a36fc0fc0d15059e76376412b8be7ba26
Cole Brokamp <cole.brokamp@gmail.com>
2023-05-18 16:27:00 -0400

    finish separating talks from abstracts

--- make.R
+++ make.R
@@ -1,17 +1,21 @@
 callr::r(\(.) rmarkdown::render_site(
-  "encoding" = "UTF-8"))
+  "encoding" = "UTF-8")) |>
+  browseURL()
 
 callr::r(\(.) rmarkdown::render(
   "_cole-brokamp-cv.Rmd",
   output_format = "pdf_document",
-  output_file = "cole-brokamp-cv.pdf"))
+  output_file = "cole-brokamp-cv.pdf")) |>
+  browseURL()
 
 callr::r(\(.) rmarkdown::render(
   '_cole-brokamp-cv-peds-format.Rmd',
   output_format = 'word_document',
-  output_file = "cole-brokamp-cv-peds-format.docx"))
+  output_file = "cole-brokamp-cv-peds-format.docx")) |>
+  browseURL()
 
 callr::r(\(.) rmarkdown::render(
   "_cole-brokamp-nih-biosketch.Rmd",
   output_format = "pdf_document",
-  output_file = "cole-brokamp-nih-biosketch.pdf"))
+  output_file = "cole-brokamp-nih-biosketch.pdf")) |>
+  browseURL()
